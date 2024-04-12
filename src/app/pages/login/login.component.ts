3357006a1c97e350c6eca1e76daf2048c32395da
ayozemt <ayozemt@gmail.com>
2024-04-12 21:13:21 +0200

    Create login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  async onSubmit() {
+    try {
+      const user = await this.userService.login({
+        email: this.email,
+        password: this.password,
+      });
+      // Login successful, navigate to home page or wherever needed
+      this.router.navigate(['/']);
+    } catch (error) {
+      // Handle login error, show error message to user
+      console.error('Login error:', error);
+    }
+  }
+}
