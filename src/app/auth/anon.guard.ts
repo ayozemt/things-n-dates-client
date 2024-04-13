2d110021056af24ed07f4e8219dbd16f7786ae94
ayozemt <ayozemt@gmail.com>
2024-04-13 13:30:58 +0200

    Create anon guard

---
+++ src/app/auth/anon.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnonGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (!this.authService.isLoggedIn()) {
+      return true; // El usuario no está autenticado, permitir acceso a la ruta
+    } else {
+      // El usuario ya está autenticado, redirigir a la página principal
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+}
